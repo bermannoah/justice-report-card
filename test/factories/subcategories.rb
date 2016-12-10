9bcb14995ed541c1e3c50f700e862513d0e09f56
Beth Sebian <esebian@gmail.com>
2016-12-09 19:43:06 -0700

    Remove score from subcategory factory

--- test/factories/subcategories.rb
+++ test/factories/subcategories.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :subcategory do
     sequence(:title) { |n| "Title #{n}" }
     sequence(:description) { |n| "Description #{n}" }
-    score 0
     category_id 1
   end
 end
